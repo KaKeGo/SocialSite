8df438f534661a6ba1b2fe39086a9acea6828056
KaKeGo <funfcia@hotmail.com>
2022-04-04 11:28:50 +0200

    add user, profile to admin pane

--- mysite/accounts/admin.py
+++ mysite/accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import User, Profile
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'last_login')
+    list_filter = ('email', 'username', 'is_staff', 'is_admin')
+    ordering = ('-date_joined',)
+    list_display = ('email', 'username')
+    fieldsets = (
+        ('Info', {'fields': ('email', 'username', 'password')}),
+        ('permissions', {'fields': ('is_superuser', 'is_admin', 'is_staff', 'is_active', 'regiment')})
+    )
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user',)
