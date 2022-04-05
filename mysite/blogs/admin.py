e4fde7ec9b138084d49b516cbd1898017e1fb059
KaKeGo <funfcia@hotmail.com>
2022-04-05 17:30:00 +0200

    add posts to admin

--- mysite/blogs/admin.py
+++ mysite/blogs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Posts
 # Register your models here.
+
+
+@admin.register(Posts)
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ('author', 'total_likes', 'create_on')
+    list_filter = ('author', 'create_on')
