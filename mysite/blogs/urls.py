f189c7b9c3433f613117a8631c7a9ff51b20bb88
KaKeGo <funfcia@hotmail.com>
2022-04-05 18:16:55 +0200

    add url for home page add login required for profile, logout

--- mysite/blogs/urls.py
+++ mysite/blogs/urls.py
@@ -1,1 +1,13 @@
+from django.urls import path
+from django.contrib.auth.decorators import login_required
 
+from .views import (
+    HomeView,
+)
+
+
+app_name = 'blogs'
+
+urlpatterns = [
+
+]
--- mysite/blogs/views.py
+++ mysite/blogs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+from django.shortcuts import resolve_url
+from django.contrib.auth.decorators import login_required
 
+from .models import Posts
 # Create your views here.
+
+
+class HomeView(generic.DetailView):
+    template_name = 'blogs/posts.html'
+    model = Posts
+    context_object_name = 'posts'
