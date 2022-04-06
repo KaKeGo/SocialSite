8586ac7f777b0307a796ecc50771f4e82dbac2d2
KaKeGo <funfcia@hotmail.com>
2022-04-06 09:33:54 +0200

    add data view for post

--- mysite/blogs/urls.py
+++ mysite/blogs/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 from .views import (
     HomeView,
+    home_post_data_view,
 )
 
 
@@ -10,4 +11,5 @@ app_name = 'blogs'
 
 urlpatterns = [
     path('', login_required(HomeView.as_view()), name='home'),
+    path('data/', home_post_data_view, name='home-data'),
 ]
--- mysite/blogs/views.py
+++ mysite/blogs/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, resolve_url
 from django.views import generic
 from django.urls import reverse_lazy
-from django.shortcuts import resolve_url
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 from .models import Posts
 # Create your views here.
@@ -12,3 +12,19 @@ class HomeView(generic.ListView):
     template_name = 'blogs/home.html'
     model = Posts
     context_object_name = 'posts'
+
+@login_required()
+def home_post_data_view(request):
+    posts = Posts.objects.all()
+
+    data = []
+    for post in posts:
+        item = {
+            'id': post.id,
+            'author': post.author.username,
+            'body': post.body,
+            'image': post.image.url,
+            'create_on': post.create_on,
+        }
+        data.append(item)
+    return JsonResponse({'data': data})
