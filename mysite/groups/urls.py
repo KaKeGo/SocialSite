393cae94ac7521f53a508b9684f082e66d838060
KaKeGo <funfcia@hotmail.com>
2022-04-10 16:02:23 +0200

    create view for groups and add to navigation

--- mysite/groups/urls.py
+++ mysite/groups/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-# from .views import (
-#
-# )
+from .views import (
+ GroupsView,
+)
 
 
 app_name = 'groups'
 
 urlpatterns = [
-
+    path('groups/', login_required(GroupsView.as_view()), name='groups'),
 ]
--- mysite/groups/views.py
+++ mysite/groups/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
+from .models import Group
 # Create your views here.
 
 
+class GroupsView(generic.ListView):
+    template_name = 'groups/groups.html'
+    model = Group
+    context_object_name = 'groups'
