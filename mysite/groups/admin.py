fe0d621ee23d4e871dec6c8dffba242684ed2c37
KaKeGo <funfcia@hotmail.com>
2022-04-10 15:12:06 +0200

    rename models 'remove model from name' add models to admin pane

--- mysite/groups/admin.py
+++ mysite/groups/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import Group, GroupPost, Regiment
+
 # Register your models here.
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'total_members', 'create_on')
+    list_filter = ('name', 'create_on')
+
+@admin.register(GroupPost)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('body', 'author')
+    list_filter = ('body', 'author')
+
+@admin.register(Regiment)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- mysite/groups/models.py
+++ mysite/groups/models.py
@@ -6,26 +6,32 @@ from accounts.models import User
 # Create your models here.
 
 
-class GroupPostModel(models.Model):
+class GroupPost(models.Model):
     body = models.TextField(max_length=500)
     image = models.ImageField(upload_to='img/group/')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class RegimentModel(models.Model):
+class Regiment(models.Model):
     name = models.CharField(max_length=50)
     body = models.TextField(max_length=200)
 
     def __str__(self):
         return str(self.name)
 
-class GroupModel(models.Model):
+class Group(models.Model):
     name = models.CharField(max_length=30, unique=True)
     founder = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(null=True, blank=True)
     members = models.ManyToManyField(User, related_name='group_members', blank=True)
-    regiment = models.ManyToManyField(RegimentModel, blank=True)
-    posts = models.ManyToManyField(GroupPostModel, blank=True)
+    regiment = models.ManyToManyField(Regiment, blank=True)
+    posts = models.ManyToManyField(GroupPost, blank=True)
     slug = models.SlugField(unique=True, null=True, blank=True)
+    create_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-name']
+        verbose_name_plural = 'Groups'
+        verbose_name = 'Group'
 
     def __str__(self):
         return str(self.name)
